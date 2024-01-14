5c230d819c10e552e04ac6dc4f647c66c11fca46
Jirasit <48820052+Tisarji@users.noreply.github.com>
2024-01-14 19:14:38 +0700

    upload form my PC

--- login-system/script.js
+++ login-system/script.js
@@ -1,3 +1,17 @@
-$('.message a').click(function(){
-	$('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+// const form = document.querySelector('form');
+// const message = document.getElementById('message');
+// const smallMessage = document.getElementById('smallMessage');
+// const emailMessage = 'Type your email';
+// const passwordMessage = 'Choose your password';
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
+const submitBtn = document.getElementById('submit');
+
+function formValidation() {
+	submitBtn.addEventListener('click', function (event) {
+		event.preventDefault();
+		window.location.href = '../home.html';
+	});
+}
+
+formValidation();
